0d33c74d7bade980747026f477529477fc4f0918
ElateTugboat <theterrarian2018@gmail.com>
2024-11-09 12:11:27 -0800

    Title and fonts folder

--- main.py
+++ main.py
@@ -5,22 +5,28 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+arial_font = pygame.font.Font("Fonts/Tangerine-Bold.ttf", 50)
+title_font_color = (255, 255, 255)
+
+title_surface = arial_font.render("Phonedle", True, title_font_color)
+
+screen_width, screen_height = screen.get_size()
+text_width, text_height = title_surface.get_size()
+
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
     bg_color = (18, 18, 76)
     screen.fill(bg_color)
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
+    
+    screen.blit(title_surface, ((screen_width - text_width) / 2, (screen_height - text_height) / 2 - 250))
+    
+    
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
+
