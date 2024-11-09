2471b9dcb44e020a0395703227aeba3a4bb4df4d
GreatLeader Technus <SCC.Cybertron555@gmail.com>
2024-11-09 11:45:49 -0800

    new file:   Clockwork.py

---
+++ Clockwork.py
@@ -0,0 +1,20 @@
+#******************************************************************
+#Programmer: Geff Freire (ZeroRX)/ZERO aka GreatLeaderTechnus     *
+#Date: November 09, 2024
+#Program: Clockwork
+#Purpose: Logic Control System for Phonedle (SCC Hacks 2024)      *
+#Inputs: Numerical Outputs from MountainDreamin's Dictionary Log Programming
+
+class Clockwork: 
+
+#Create a Dictionary to hold values of
+    def __init__(self, name, phonNum):
+        self.name = name
+        self.phonNum = phonNum
+
+#Build the dictionary of the phone number and names
+    def BuildNum (name, phonNum):
+        Contact = {phonNum:name}
+
+        for num i
+
