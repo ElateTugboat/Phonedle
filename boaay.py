c571d9b71f4c182dbaddc594bb9862bd1f5e134c
BOAAY <blaketheboss1.0@gmail.com>
2024-11-09 13:57:32 -0800

    boxes

--- boaay.py
+++ boaay.py
@@ -3,17 +3,33 @@ import pygame
 def BoaayFunc(screen):
     
     numpad = pygame.image.load("art/numpad.png").convert()
+    
 
-    numpad = pygame.transform.scale(numpad, (250, 250))
+    numpad = pygame.transform.scale(numpad, (200, 200))
 
-    screen.blit(numpad, ((550,400)))
+    screen.blit(numpad, ((550,450)))
+
+    DrawBoxLine([100,350], 10, 100, screen)
+
+
+def DrawBoxLine(pos, lenth, step, screen):
+
+    i = 0
+    
+    scale_both = 80
+
+    scale = (scale_both, scale_both)
+
+    while i < lenth:
+        pos[0] = pos[0] + step
+        
+        blank_box = pygame.image.load("art/blank.png").convert()
 
     
-    blank_box = pygame.image.load("art/blank.png").convert()
 
-    blank_box = pygame.transform.scale(blank_box, (250, 250))
 
-    screen.blit(blank_box, ((0,0)))
+        blank_box = pygame.transform.scale(blank_box, scale)
 
-    # test
+        screen.blit(blank_box, ((pos[0] , pos[1] )))
+        i += 1
 
